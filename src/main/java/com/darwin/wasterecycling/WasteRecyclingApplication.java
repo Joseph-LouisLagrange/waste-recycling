462100adbb101ddbffe6926ffafd8801ee3a71d3
darwinyang <2580012389@qq.com>
2021-09-03 19:30:09 +0800

    init

---
+++ src/main/java/com/darwin/wasterecycling/WasteRecyclingApplication.java
@@ -0,0 +1,13 @@
+package com.darwin.wasterecycling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteRecyclingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WasteRecyclingApplication.class, args);
+    }
+
+}
