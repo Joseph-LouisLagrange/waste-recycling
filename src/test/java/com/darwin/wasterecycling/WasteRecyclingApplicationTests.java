d23f4fa4a7d98330fac9102294e98d6d44f242e5
darwinyang <2580012389@qq.com>
2021-09-03 19:49:54 +0800

    update:修改 pom 依赖

--- src/test/java/com/darwin/wasterecycling/WasteRecyclingApplicationTests.java
+++ src/test/java/com/darwin/wasterecycling/WasteRecyclingApplicationTests.java
@@ -1,5 +1,6 @@
 package com.darwin.wasterecycling;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class WasteRecyclingApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
